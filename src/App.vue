ebcc0128e4941e8a12b93500e51e3ea37ee9aedb
Alexander Gonzalez <alexfertel97@gmail.com>
2020-01-24 15:57:15 -0500

    Finish tabs, refactor some css, add hover features

--- src/App.vue
+++ src/App.vue
@@ -5,21 +5,19 @@
       <div class="ml-6 mt-6 flex items-center">
         <div class="flex items-center">
           <img
-            class="h-10 w-h-10 rounded-full"
+            class="h-12 w-h-12 rounded-full"
             src="/img/profile.jpg"
             alt="profile"
           />
-          <span class="ml-8 text-lg text-gray-100 font-extrabold">
+          <span class="ml-8 text-2xl text-gray-200 font-extrabold">
             Alexander González
           </span>
         </div>
         <svg
-          class="h-6 w-6 text-white sm:hidden"
+          class="h-6 w-6 text-white sm:hidden fill-current"
           viewBox="0 0 24 24"
         >
           <path
-            stroke="currentColor"
-            stroke-width="2"
             d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"
           />
         </svg>
@@ -27,12 +25,6 @@
 
       <!-- Tabs -->
       <Tabs/>
-
-      <!-- About-->
-      <p class="">
-        Hello there! I am a 22 years old computer scientist graduated from 
-        the University of Havana.
-      </p>
 
     </div>
   </div>
--- src/components/Tabs.vue
+++ src/components/Tabs.vue
@@ -1,20 +1,90 @@
 <template>
   <div>
+    <!-- Headers -->
     <ol class="mt-6 mx-2 flex">
       <li
         v-for="(item, index) in items"
         :key="index"
-        class="ml-6 cursor-pointer hover:border-blue-800"
+        class="ml-6 cursor-pointer"
         @click="active = index"
       >
         <h3
-          class="text-gray-400 font-semibold tracking-wide"
-          :class="active == index ? 'border-b-2 border-solid border-red-500' : ''"
+          class="text-gray-300 font-semibold tracking-wide hover:text-red-600"
+          :class="
+            active == index ? 'border-b-2 border-solid border-red-700' : ''
+          "
         >
           {{ item.title }}
         </h3>
       </li>
     </ol>
+
+    <!-- About -->
+    <div class="tab" :class="active != 0 ? 'hidden' : 'block'">
+      <p class="tab-content">
+        Hello there! <br />
+        I'm a {{ age }} years old computer scientist graduated from the
+        University of Havana.
+      </p>
+    </div>
+
+    <!-- Work -->
+    <div class="tab" :class="active != 1 ? 'hidden' : 'block'">
+      <p class="tab-content">
+        I have some experience in
+        <span
+          class="red-link"
+          href="https://en.wikipedia.org/wiki/Web_development"
+        >
+          Web Development </span
+        >,
+        <span
+          class="red-link"
+          href="https://en.wikipedia.org/wiki/Machine_learning"
+        >
+          Machine Learning
+        </span>
+        and
+        <span
+          class="red-link"
+          href="https://en.wikipedia.org/wiki/Procedural_generation"
+        >
+          Procedural Content Generation
+        </span>
+        .
+      </p>
+
+      I've combined the last two in my bachelor's thesis
+      <span
+        class="red-link"
+        href="https://github.com/alexfertel/wfc"
+      >
+        here
+      </span>
+      .
+    </div>
+
+    <!-- Contact -->
+    <div class="tab" :class="active != 2 ? 'hidden' : 'block'">
+      <p class="tab-content">
+        <span class="flex items-center">
+          <svg class="h-6 w-6 fill-current" viewBox="0 0 24 24">
+            <path
+              d="M4 4h16a2 2 0 012 2v12a2 2 0 01-2 2H4a2 2 0 01-2-2V6c0-1.1.9-2 2-2zm16 3.38V6H4v1.38l8 4 8-4zm0 2.24l-7.55 3.77a1 1 0 01-.9 0L4 9.62V18h16V9.62z"
+            />
+          </svg>
+          <span class="ml-2">alexfertel97@gmail.com</span>
+        </span>
+        <span class="mt-2 flex items-center">
+          <svg class="h-6 w-6 fill-current" viewBox="0 0 512 512">
+            <path
+              d="M256 0C114.636719 0 0 114.636719 0 256s114.636719 256 256 256 256-114.636719 256-256S397.363281 0 256 0zm116.886719 199.601562c.113281 2.519532.167969 5.050782.167969 7.59375 0 77.644532-59.101563 167.179688-167.183594 167.183594h.003906-.003906c-33.183594 0-64.0625-9.726562-90.066406-26.394531 4.597656.542969 9.277343.8125 14.015624.8125 27.53125 0 52.867188-9.390625 72.980469-25.152344-25.722656-.476562-47.410156-17.464843-54.894531-40.8125 3.582031.6875 7.265625 1.0625 11.042969 1.0625 5.363281 0 10.558593-.722656 15.496093-2.070312-26.886718-5.382813-47.140624-29.144531-47.140624-57.597657 0-.265624 0-.503906.007812-.75 7.917969 4.402344 16.972656 7.050782 26.613281 7.347657-15.777343-10.527344-26.148437-28.523438-26.148437-48.910157 0-10.765624 2.910156-20.851562 7.957031-29.535156 28.976563 35.554688 72.28125 58.9375 121.117187 61.394532-1.007812-4.304688-1.527343-8.789063-1.527343-13.398438 0-32.4375 26.316406-58.753906 58.765625-58.753906 16.902344 0 32.167968 7.144531 42.890625 18.566406 13.386719-2.640625 25.957031-7.53125 37.3125-14.261719-4.394531 13.714844-13.707031 25.222657-25.839844 32.5 11.886719-1.421875 23.214844-4.574219 33.742187-9.253906-7.863281 11.785156-17.835937 22.136719-29.308593 30.429687zm0 0"
+            />
+          </svg>
+          <span class="ml-2">alexagf97</span>
+        </span>
+      </p>
+    </div>
   </div>
 </template>
 
@@ -23,10 +93,26 @@ export default {
   data() {
     return {
       active: 0,
+      age: 22, // This should be updated automatically using `momentjs`.
       items: [
-        { title: "About", active: true },
-        { title: "Work", active: false },
-        { title: "Contact", active: false }
+        {
+          title: "About",
+          active: true,
+          content: `Hello there! <br/> I am a 22 years old computer scientist graduated from
+            the University of Havana.`
+        },
+        {
+          title: "Work",
+          active: false,
+          content: `I have some experience in Web Development, Machine Learning and 
+            Procedural Content Generation.`
+        },
+        {
+          title: "Contact",
+          active: false,
+          content: `Email: alexfertel97@gmail.com <br/>
+                    Twitter: @alexagf97`
+        }
       ]
     };
   }
